2a11673833c0b5ca9b4e55b12b0622edc3b65589
phanindra <phanindrabompalli@gmail.com>
2023-03-03 17:30:47 +0000

    apiRouting

---
+++ src/main/java/com/example/apirouting/MyService.java
@@ -0,0 +1,14 @@
+// Write your code here. 
+package com.example.apirouting;   
+
+public class MyService{ 
+
+    public String getHomePage(){
+        return "Home Page";
+    }
+    
+    public String getAboutPage(){
+        return "About Page";
+    }
+}
+
